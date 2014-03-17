20e8cb6f86d77e68aba98b6ea5ed7acfcb99e42e
Troy Anderson <the.inventor@gmail.com>
2014-03-17 14:05:58 -0700

    updates to report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -12,7 +12,7 @@ class ReportsController < ApplicationController
     if params[:month]
       commissions = Commission.where(month: params[:month])
       start_date = Date.strptime("#{params[:month]} 1 2014", "%B %d %Y")
-      customer_ids = Customer.where('start_date < ?',start_date+2.day).pluck(:id)
+      customer_ids = params[:carrier] ? Customer.where('start_date < ?',start_date+2.day).where(carrier: params[:carrier]).pluck(:id) : Customer.where('start_date < ?',start_date+2.day).pluck(:id)
       missing_customer_ids = customer_ids-commissions.pluck(:customer_id)
       customers = Customer.where(id: missing_customer_ids)
       @results = customers
