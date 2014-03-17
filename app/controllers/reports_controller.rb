a003b32a4da32723f2eef0459110239c14fffb71
Troy Anderson <the.inventor@gmail.com>
2014-03-17 13:43:31 -0700

    fix date logic for real

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -9,11 +9,10 @@ class ReportsController < ApplicationController
   end
 
   def month
-
     if params[:month]
       commissions = Commission.where(month: params[:month])
       start_date = Date.strptime("#{params[:month]} 1 2014", "%B %d %Y")
-      customer_ids = Customer.where('start_date > ?',start_date-1.day).pluck(:id)
+      customer_ids = Customer.where('start_date < ?',start_date+2.day).pluck(:id)
       missing_customer_ids = customer_ids-commissions.pluck(:customer_id)
       customers = Customer.where(id: missing_customer_ids)
       @results = customers
